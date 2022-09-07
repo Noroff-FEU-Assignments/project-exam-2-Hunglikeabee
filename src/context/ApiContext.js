9b8c22d23368c04bf97645ff99954a2f60899756
Hunglikeabee <felix_256@hotmail.com>
2022-09-07 11:55:04 +0200

    Headhurtsworkplz

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import useLocalStorage from './../hooks/useLocalStorage';
-=======
-import React, { useState } from "react";
->>>>>>> e3ef20bba6e6e8665ebd60adc5001051a363fa24
 
 const ApiContext = React.createContext([[], () => {}]);
 
 export const ApiProvider = (props) => {
-<<<<<<< HEAD
   const [apiData, setApi] = useLocalStorage("hotelsList", []);
   useEffect(() => {
     (async () => {
@@ -31,10 +26,4 @@ export const ApiProvider = (props) => {
     </ApiContext.Provider>
   );
 };
-=======
-    const [apiData, setApi] = useState([]);
-    return <ApiContext.Provider value={[apiData, setApi]}>{props.children}</ApiContext.Provider>
-}
-
->>>>>>> e3ef20bba6e6e8665ebd60adc5001051a363fa24
 export default ApiContext;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Paragraph from "../components/general/Paragraph"
 import SearchBar from "../components/general/SearchBar"
 import Hero from "../components/HomePage/HeroImage"
 import HotelPreview from "../components/HotelsPage/HotelPreview"
-<<<<<<< HEAD
 import { useContext } from "react"
 import ApiContext from "../context/ApiContext"
 
@@ -13,10 +12,8 @@ export default function Home(props) {
 
     const [apiData] = useContext(ApiContext)
 
-=======
 
 export default function Home(props) {
->>>>>>> e3ef20bba6e6e8665ebd60adc5001051a363fa24
     return(
         <>
         <HeadingH1Style>holidaze your life</HeadingH1Style>
@@ -24,11 +21,8 @@ export default function Home(props) {
         <Paragraph>Where your dreams and our vision meet!</Paragraph>
         <Hero></Hero>
         <DefaultButton>HEY</DefaultButton>
-<<<<<<< HEAD
         <HotelPreview data={apiData} />
-=======
         <HotelPreview />
->>>>>>> e3ef20bba6e6e8665ebd60adc5001051a363fa24
         </>
 
     )
